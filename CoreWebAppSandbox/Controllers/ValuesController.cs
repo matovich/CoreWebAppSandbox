30f922a7bd2b416be90fe31bfd8481188b5a48d5
DOCUSIGNHQ\Paul.Matovich <paul.matovich@docusign.com>
2019-05-01 13:16:52 -0500

    Add data store

--- CoreWebAppSandbox/Controllers/ValuesController.cs
+++ CoreWebAppSandbox/Controllers/ValuesController.cs
@@ -14,32 +14,37 @@ namespace CoreWebAppSandbox.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            if (!DataStore.GetAll().Any())
+                return new[] { "There are no values." };
+            return new ActionResult<IEnumerable<string>>(DataStore.GetAll());
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return DataStore.GetIndex(id);
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            DataStore.Add(value);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+            DataStore.SetIndex(id, value);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            DataStore.Delete(id);
         }
     }
 }
---
+++ CoreWebAppSandbox/DataStore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebAppSandbox
+{
+    public static class DataStore
+    {
+        private static readonly List<string> Data = new List<string>();
+
+        public static IEnumerable<string> GetAll()
+        {
+            return Data;
+        }
+
+        public static string GetIndex(int index)
+        {
+            return Data[index];
+        }
+
+        public static void SetIndex(int index, string value)
+        {
+            Data[index] = value;
+        }
+
+        public static void Add(string value)
+        {
+            Data.Add(value);
+        }
+
+        public static void Delete(int index)
+        {
+            Data.RemoveAt(index);
+        }
+    }
+}
